b4c8f5674f542c66326ae2b6916f4d5bf894dd6f
Bappy38 <iqbal.bappy.13@gmail.com>
2025-04-15 16:30:19 +0600

    NavBar should exist in Login and SignUp page also

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,23 @@ import Profile from "./components/Profile";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import appStore from "./store/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body/>}>
-            <Route path="profile" element={<Profile/>} />
-          </Route>
-          <Route path="login" element={<Login/>} />
-          <Route path="signup" element={<SignUp/>} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body/>}>
+              <Route path="profile" element={<Profile/>} />
+              <Route path="login" element={<Login/>} />
+              <Route path="signup" element={<SignUp/>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
