d9eb3d36f7f536b4f80b72eda39d5533b5fbf14a
Bappy38 <iqbal.bappy.13@gmail.com>
2025-04-15 13:23:39 +0600

    axios package installed and Login component implemented

--- Tasks.md
+++ Tasks.md
@@ -6,3 +6,7 @@
 - Install `daisyUI` as a `TailwindCSS` plugin
 - Install `react-router-dom`
 - Create routes using BrowserRouter (Declarative Mode)
+- Create Login Page
+- Install axios
+- Install `cors` and use middleware with `origin` and `credentials` configuration in backend
+- Whenever you're making API call through axios, pass `{withCredentials: true}`
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,86 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = () => {
+    const [ email, setEmail ] = useState("");
+    const [ password, setPassword ] = useState("");
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        
+        try {
+            const response = await axios.post("http://localhost:3000/auth/signin",
+            {
+                email,
+                password
+            },
+            {
+                withCredentials: true
+            });
+            console.log(response.data);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    const isFormValid = () => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+        
+        return emailRegex.test(email) && passwordRegex.test(password);
+    };
+
     return (
-        <div>
-            Login Page
+        <div className="flex justify-center mt-20">
+
+            <div className="card w-96 bg-base-100 shadow-sm">
+                <div className="card-body">
+                    <form onSubmit={handleLogin}>
+                        <div>
+                            <label className="input validator">
+                                <input 
+                                    value={email} 
+                                    onChange={(e) => setEmail(e.target.value)} 
+                                    type="email" 
+                                    placeholder="Enter your email" 
+                                    required
+                                />
+                            </label>
+                            <div className="validator-hint hidden">Enter valid email address</div>
+                        </div>
+
+                        <div className="mt-4">
+                            <label class="input validator">
+                                <input
+                                    value={password} 
+                                    onChange={(e) => setPassword(e.target.value)} 
+                                    type="password" 
+                                    required 
+                                    placeholder="Password" 
+                                    minlength="8" 
+                                    pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                                    title="Must be more than 8 characters, including number, lowercase letter, uppercase letter" 
+                                />
+                            </label>
+                            <p class="validator-hint hidden">
+                            Must be more than 8 characters, including
+                            <br/>⬩ At least one number
+                            <br/>⬩ At least one lowercase letter
+                            <br/>⬩ At least one uppercase letter
+                            </p>
+                        </div>
+
+                    
+                        <div className="mt-6 flex justify-center">
+                            <button
+                                type="submit"
+                                className="btn btn-primary w-[60%]"
+                                disabled={!isFormValid()}
+                            >Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
