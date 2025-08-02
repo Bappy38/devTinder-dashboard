cc8d58f8c31e32acfd89b586d4606a84857f73e7
Bappy38 <iqbal.bappy.13@gmail.com>
2025-08-02 17:54:25 +0600

    Unauthorized room access and send message issue resolved

--- src/hooks/useConnectChat.js
+++ src/hooks/useConnectChat.js
@@ -1,10 +1,13 @@
 import { useEffect, useState, useRef } from "react"
 import { createSocketConnection } from "../helpers/socket"
 import { EVENTS } from "../constants/events";
+import { NOTIFICATION_TYPE } from "../constants/notificationType";
+import { useNavigate } from "react-router-dom";
 
 const useConnectChat = (roomId) => {
     const [messages, setMessages] = useState([]);
     const socketRef = useRef();
+    const navigate = useNavigate();
 
     useEffect(() => {
         socketRef.current = createSocketConnection();
@@ -14,7 +17,12 @@ const useConnectChat = (roomId) => {
             socket.connect();
         }
 
-        socket.emit(EVENTS.JOIN_ROOM, roomId);
+        socket.emit(EVENTS.JOIN_ROOM, { roomId }, (response) => {
+            if (response.unAuthorized) {
+                window.showNotification(crypto.randomUUID(), "You do not have access to this room", NOTIFICATION_TYPE.ERROR);
+                navigate('/');
+            }
+        });
 
         socket.on(EVENTS.RECEIVE_MESSAGE, (msg) => {
             setMessages((prev) => [...prev, msg]);
@@ -28,7 +36,11 @@ const useConnectChat = (roomId) => {
 
     const sendMessage = (text) => {
         if (socketRef.current) {
-            socketRef.current.emit(EVENTS.SEND_MESSAGE, { roomId, text });
+            socketRef.current.emit(EVENTS.SEND_MESSAGE, { roomId, text }, (response) => {
+                if (response.error) {
+                    window.showNotification(crypto.randomUUID(), response.error, NOTIFICATION_TYPE.ERROR);
+                }
+            });
         }
     }
 
